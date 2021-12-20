997dd2a78f88bfbcccccfbeff21676fce7af6a3d
mvaldes14 <mvaldes.revilla@gmail.com>
2021-12-20 09:17:04 -0600

    turned everything into functions

--- scrape.rb
+++ scrape.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 require 'httparty'
 require 'nokogiri'
 require 'sqlite3'
@@ -5,46 +6,57 @@ require 'telebot'
 require 'logger'
 
 # Init DB
-db = SQLite3::Database.open 'tracker.db'
-db.execute 'CREATE TABLE IF NOT EXISTS products(product TEXT, added TEXT)'
-db.results_as_hash = true
-
-# Define Log
-log = Logger.new('scraper.log')
-log.level = Logger::INFO
-
-# Telegram settings
-token = ENV['TELEGRAM_TOKEN']
-chat_id = ENV['TELEGRAM_CHAT']
-if token.nil?
-  log.fatal 'Token not loaded, aborting'
-  abort
-end
-client = Telebot::Client.new(token)
-
-# Check if client started correctly
-if client.get_me.nil?
-  log.error 'Bot not initialized'
+def init_db
+  db = SQLite3::Database.open 'tracker.db'
+  db.execute 'CREATE TABLE IF NOT EXISTS products(product TEXT, added TEXT)'
+  db.results_as_hash = true
+  db
 end
 
-# Scrape results from site
-log.info "Starting script at: #{Time.now}"
-url = 'https://www.freegeektwincities.org/computers'
-request = HTTParty.get(url)
-html = Nokogiri::HTML(request)
-products = html.css('.ProductList-title')
-log.info "Found #{products.length} products in site"
-
-# Validate and Save
-products.children.each do |p|
-  check_if_db = db.query 'SELECT * FROM products WHERE product = ?', p.inner_text
-  log.info "Checking if #{p.inner_text} exists in the database"
-  if check_if_db.count.zero?
-    # Save to DB, notify and log
-    log.info "Server added to DB: #{p.inner_text}"
-    client.send_message(chat_id: chat_id, text: "NEW SERVER POSTED:#{p.inner_text}")
-    db.execute 'INSERT INTO products(product, added) VALUES (?,?)', p.inner_text, Time.now.to_s
-  else
-    log.info 'Server already in the db, skipping'
+def init_telegram(token)
+  if token.nil?
+    log.fatal 'Token not loaded, aborting'
+    abort
   end
+  client = Telebot::Client.new(token)
+  # Check if client started correctly
+  log.error 'Bot not initialized' if client.get_me.nil?
+  client
+end
+
+def main(db, client, chat_id)
+  # Define Log
+  log = Logger.new('scraper.log')
+  log.level = Logger::INFO
+  # Scrape results from site
+  log.info "Starting script at: #{Time.now}"
+  url = 'https://www.freegeektwincities.org/computers'
+  request = HTTParty.get(url)
+  html = Nokogiri::HTML(request)
+  products = html.css('.ProductList-title')
+  log.info "Found #{products.length} products in site"
+
+  # Validate and Save
+  products.children.each do |p|
+    check_if_db = db.query 'SELECT * FROM products WHERE product = ?', p.inner_text
+    log.info "Checking if #{p.inner_text} exists in the database"
+    if check_if_db.count.zero?
+      # Save to DB, notify and log
+      log.info "Server added to DB: #{p.inner_text}"
+      client.send_message(chat_id: chat_id, text: "NEW SERVER POSTED:#{p.inner_text}")
+      db.execute 'INSERT INTO products(product, added) VALUES (?,?)', p.inner_text, Time.now.to_s
+    else
+      log.info 'Server already in the db, skipping'
+    end
+  end
+  client.send_message(chat_id: chat_id, text: "NO NEW SERVER FOUND at #{Time.now.to_s}")
+end
+
+if $PROGRAM_NAME == __FILE__
+  # Telegram settings
+  token = ENV['TELEGRAM_TOKEN']
+  chat_id = ENV['TELEGRAM_CHAT']
+  db = init_db()
+  client = init_telegram(token)
+  main(db, client, chat_id)
 end
